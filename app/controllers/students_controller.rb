14d1825e4409a1007ca6bbf8816fb23294930e6a
ronald vilorio <soccer23415@gmail.com>
2018-04-23 18:14:24 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,10 @@
 class StudentsController < ApplicationController
   def create
+<<<<<<< HEAD
     @student = Student.new(post_params(:first_name,:last_name))
+=======
+    @student = Student.new(post_params(:title,:room_number))
+>>>>>>> 81c1d8c3976a1882e56d41819b16d665fd9582bc
   	@student.save
   	redirect_to student_path(@student)
   end
@@ -10,6 +14,7 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+<<<<<<< HEAD
   def edit
     @student = Student.find(params[:id])
   end
@@ -19,6 +24,8 @@ class StudentsController < ApplicationController
   	redirect_to student_path(@student)
 
   end
+=======
+>>>>>>> 81c1d8c3976a1882e56d41819b16d665fd9582bc
   private
 		def post_params(*args)
   		params.require(:student).permit(*args)
